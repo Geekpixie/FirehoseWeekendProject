bcbacddf8aa974b7eda2b463908fdbc5733775ad
Amber Lee <amber.lee@rightscale.com>
2013-09-14 18:01:23 -0400

    Adding form fields to the pics page

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,26 @@
+class PicsController < ApplicationController
+	#add new here
+	def new
+		#hello ruby code here
+		@pic = Pic.new
+	end
+
+	def index
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+end
